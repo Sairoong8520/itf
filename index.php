00e2549719965fb73638e1582dfa6544cfa7d542
Sairoong8520 <73654933+Sairoong8520@users.noreply.github.com>
2020-10-30 13:31:53 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
-<?php
-echo "<h2>IT@KMITL</h2>";
-echo "Hello IT<br>";
-?>
+<html>
+    <head><title>Lab ITF</title>
+    </head>
+    <body>
+        <p>ชื่อ : นางสาวพิมพ์มาดา กรุ่นสูงเนิน</p>
+        <p>ชื่อเล่น : สายรุ้ง</p>
+        <br>
+        <a href="https://www.ygfamily.com/">This is a Link</a>
+        <p>มิสเตอร์แวน เป็นหมอ ที่ถูกส่งไปทำงานในยุโรป พอไปถึงที่ เขาก็ส่งโทรเลขถึงภรรยาที่อยู่ถึงอเมริกา </p>
+        <p>แต่บังเอิญบุรุษไปรษณีส่งผิด เขานำโทรเลขไปส่งให้บ้านคุณนายแวนอีกคนหนึ่งซึ่งสามีเพิ่งเสียชีวิตไปเมื่อวานนี้เอง </p>
+        <p>ทันทีเปิดโทรเลขอ่าน คุณนายแวนก็แทบจะหัวใจวายเนื่องจากโทรเลขมีข้อความว่า </p>
+        <p>...ถึงที่หมายโดยปลอดภัยเช้าวันนี้เอง หวังว่าคุณคงจะตามผมมาเร็วๆนี้...</p>
+        <img src="https://www.bangkokpost.com/media/content/dcx/2020/08/17/3722547.jpg">
+        <img src="https://cdn.vox-cdn.com/thumbor/04akU2Egj016Z1tj5E8TicSiBpA=/0x7:960x727/1400x1400/filters:focal(0x7:960x727):format(jpeg)/cdn.vox-cdn.com/uploads/chorus_image/image/50522919/cat-cafe-la.0.0.jpg">
+    </body>
+</html>
