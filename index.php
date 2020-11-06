16a816684c2ef89fc3c943ce2c0e63d6d4fa6329
Sairoong8520 <73654933+Sairoong8520@users.noreply.github.com>
2020-11-06 12:24:52 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<head></head>
+    <title>LAB ITF</title>
+<body>
+    <h3>Work2 / Work3 06/11/2020</h3>
+    <a href="work2,3/Lab5.11.html">work2</a><br>
+    <a href="work2,3/lab.card1.html">work3</a>
+</body>
