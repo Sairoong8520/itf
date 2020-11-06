9ffda77c85b5c93e01c1d1319244d907eac4f9f4
Sairoong8520 <73654933+Sairoong8520@users.noreply.github.com>
2020-11-06 15:36:51 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <!DOCTYPE html>
-<head></head>
+<html>
+<head>
     <title>LAB ITF</title>
+</head>
 <body>
     <h3>Work2 / Work3 06/11/2020</h3>
     <a href="work2,3/Lab5.11.html">work2</a><br>
     <a href="work2,3/lab.card1.html">work3</a>
 </body>
+</html>
